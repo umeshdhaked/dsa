1a6aee1613e3c19a63c094d40b95d637d7e97d55
umeshdhaked <umdk456@gmail.com>
2026-02-06 21:41:17 +0530

    graph problem

---
+++ commons/r_graphs/CountCompleteComponents.java
@@ -0,0 +1,54 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class CountCompleteComponents {
+    
+    public int countCompleteComponents(int n, int[][] edges) {
+
+        List<List<Integer>> adjList = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            adjList.add(new ArrayList<>());
+        }
+
+        for (int[] edge : edges) {
+            adjList.get(edge[0]).add(edge[1]);
+            adjList.get(edge[1]).add(edge[0]);
+        }
+
+        boolean[] visitedComp = new boolean[n];
+        Queue<Integer> q = new LinkedList<>();
+
+        int ans = 0;
+        for (int i = 0; i < n; i++) {
+
+            int numberOfNode = 0;
+            int numberOfEdge = 0;
+
+            if (!visitedComp[i]) {
+                q.add(i);
+
+                while (!q.isEmpty()) {
+                    int startNode = q.poll();
+                    if (!visitedComp[startNode]) {
+                        numberOfNode++;
+                        visitedComp[startNode] = true;
+                        List<Integer> endNodeOfEdge = adjList.get(startNode);
+                        numberOfEdge = numberOfEdge + endNodeOfEdge.size();
+                        q.addAll(endNodeOfEdge);
+                    }
+
+                }
+                
+                if (numberOfNode*(numberOfNode-1) == numberOfEdge) {
+                    ans++;
+                }
+
+            }
+        }
+
+        return ans;
+    }
+
+}
